39321c8d7067fc71cf8bb125f8a1adaa98daa3eb
Student <student@Admins-MacBook-Air-5.local>
2018-05-19 13:08:28 -0700

    Stuff

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* write your script here */ 
+let database = [{
+  Make: "BMW",
+  Model: "540I",
+  Hp: 350-400,
+  Year: "2001",
+},{
+  Make: "Mercedez-Benz",
+  Model: "E55-AMG",
+  Hp: 350-400,
+  Year: "2006",
+  }]
+
+let searchBar = document.getElementById('search-bar');
+let searchButton;= document.getElementById('search-button');
+let autoSuggestions;= document.getElementById('search-suggestions');
+let display;= document.getElementById('display');
+
+searchBar.addeventListener("keypress", checkKey);
+
+
+function checkKey(e) {
+  var key = e.which || e.keyCode;
+  if(key == 13){
+    processInput();
+  }
+}
+
+
+function processInput() {
+  
+}
